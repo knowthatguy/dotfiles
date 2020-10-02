8289dc77ba44fabc6c2258eb369579851417f966
Oleksii Novosadovskyi <o.novosadovskyi@gmail.com>
2020-10-02 12:39:20 +0300

    Build 2020

---
+++ config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+export PATH=~/bin:$PATH
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+# polybar i3wmthemer_bar &
+polybar main &
+
